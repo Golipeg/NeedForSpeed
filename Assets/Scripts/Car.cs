4a5ca3bce2a76fc8e38d96e9e693264ac08dc7f3
Golipeg <golipeg@gmail.com>
2022-09-29 22:27:29 +0300

    Upgrage Road V2

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    
+
+    void Start()
+    {
+        Debug.Log("Car is Ready!");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
