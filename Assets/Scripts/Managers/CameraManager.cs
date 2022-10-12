5ee4340860a51819d61d01ea84d2c9d035154d12
Golipeg <golipeg@gmail.com>
2022-10-13 00:06:23 +0300

    RebuildGame Need For Speed

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -7,16 +7,16 @@ public class CameraManager : MonoBehaviour
 {
     [SerializeField] private float _yOffset = 0f;
     [SerializeField] private float _zOffset = 0f;
-    [SerializeField] private CarManager _carManager;
+    [SerializeField] private Transform _car;
     [SerializeField] private Camera _camera;
     
     private void LateUpdate()
     {
-        if (!_carManager.CarTransform)
+        if (!_car.transform)
         {
             return;
         }
-        _camera.transform.position = new Vector3(_carManager.CarTransform.position.x,_carManager.CarTransform.position.y+_yOffset,_carManager.CarTransform.position.z+_zOffset);
+        _camera.transform.position = new Vector3(_car.transform.position.x,_car.transform.position.y+_yOffset,_car.transform.position.z+_zOffset);
          
     }
 }
